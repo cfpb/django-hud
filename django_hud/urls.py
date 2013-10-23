d1e5fedc77384bd8342419a52892def095b0f00d
S Gorobet <Serghei.Gorobet@cfpb.gov>
2013-10-23 15:54:47 -0400

    remove easy_test and cron_job url settings

--- django_hud/urls.py
+++ django_hud/urls.py
@@ -13,8 +13,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^hud-api-replace/', include('hud_api_replace.urls')),
-    url(r'^cron-job/', include('cron_job.urls')),
-    url(r'^easy-test/', include('easy_test.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
