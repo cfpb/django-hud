13eb0ec6ae9587430969b2963676daf30536a2c5
Ross Karchner <ross.karchner@cfpb.gov>
2017-06-08 10:16:45 -0400

    minimal change to use the permanenet geocoding API

--- hud_api_replace/geocode.py
+++ hud_api_replace/geocode.py
@@ -1,18 +1,27 @@
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 
-import json
-import hmac
-import base64
-import hashlib
 import geocoder
-from geocoder.mapbox import Mapbox
+from geocoder.mapbox import Mapbox, mapbox_access_token
 
 import re
 import time
 
 from hud_api_replace.models import CachedGeodata
 
+
+class PermanentMapbox(Mapbox):
+    def __init__(self, location, **kwargs):
+        self.location = location
+        self.url = u'https://api.mapbox.com/geocoding/v5/mapbox.places-permanent/{0}.json'.format(location)
+        self.params = {
+            'access_token': self._get_api_key(mapbox_access_token, **kwargs),
+            'country': kwargs.get('country'),
+            'proximity': self._get_proximity(),
+            'types': kwargs.get('types'),
+        }
+        self._get_proximity(**kwargs)
+        self._initialize(**kwargs)
 
 
 def _extract_zip_code(key):
@@ -25,18 +34,19 @@ def _extract_zip_code(key):
 
 def geocode_get_data(address, zip_only=False):
     """Main function to obtain geocoding information."""
-    address = str(address) + ", usa"
     try:
         cached = CachedGeodata.objects.all().filter(expires__gte=time.time()).get(key=address)
     except ObjectDoesNotExist:
         if zip_only:
-            mb_result = Mapbox(address ,types='postcode')
+            mb_result = PermanentMapbox(address,
+                                        types='postcode',
+                                        country='us')
             if mb_result.latlng:
                 result = mb_result
             else:
                 result = geocoder.osm(address)
         else:
-            result = geocoder.mapbox(address)
+            result = PermanentMapbox(address, country="us")
         cached, created = CachedGeodata.objects.get_or_create(key=address)
         cached.lat = result.lat
         cached.lon = result.lng
